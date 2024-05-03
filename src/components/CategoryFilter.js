126fe341dceee9f47991c2772cbc40d29797a7dd
Abdulhakim Kullow <abdulhakim.kullow@student.moringaschool.com>
2024-05-03 15:09:44 +0300

    completed lab

--- src/components/CategoryFilter.js
+++ src/components/CategoryFilter.js
@@ -1,10 +1,26 @@
 import React from "react";
 
-function CategoryFilter() {
+function CategoryFilter({ onSelectCategory, selectedCategory }) {
+  // Define an array of categories
+  const categories = ["All", "Code", "Food", "Money", "Misc"];
+
   return (
     <div className="categories">
       <h5>Category filters</h5>
-      {/* render <button> elements for each category here */}
+      {/* Map over the categories array */}
+      {categories.map((category) => (
+        // Render a button element for each category
+        <button
+          key={category}
+          // Call the onSelectCategory function with the current category as an argument when the button is clicked
+          onClick={() => onSelectCategory(category)}
+          // Add the "selected" class to the button if the selectedCategory is equal to the current category
+          className={selectedCategory === category ? "selected" : ""}
+        >
+          {/* Display the category name */}
+          {category}
+        </button>
+      ))}
     </div>
   );
 }
